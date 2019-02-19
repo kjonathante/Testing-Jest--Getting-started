aac626009ff629c1e24a6562587f4ac9e1cedea4
Kit Jonathan Te <36757045+kjonathante@users.noreply.github.com>
2019-02-19 10:36:49 -0800

    Added Async/Await tests

--- promise.test.js
+++ promise.test.js
@@ -11,3 +11,13 @@ test("testing promise using then", () => {
 test("testing promise using resolves", () => {
   return expect(fetchData()).resolves.toBe("peanut butter");
 });
+
+test("testing promise using async/await", async () => {
+  expect.assertions(1);
+  const data = await fetchData();
+  expect(data).toBe("peanut butter");
+});
+
+test("testing promise using async/await and resolves", async () => {
+  await expect(fetchData()).resolves.toBe("peanut butter");
+});
--- promiseReject.test.js
+++ promiseReject.test.js
@@ -8,3 +8,16 @@ test("the fetch fails with an error using catch", () => {
 test("the fetch fails with an error using rejects", () => {
   return expect(fetchData()).rejects.toMatch("error");
 });
+
+test("the fetch fails with an error using async/await + catch", async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch("error");
+  }
+});
+
+test("the fetch fails with an error using async/await + rejects", async () => {
+  await expect(fetchData()).rejects.toMatch("error");
+});
